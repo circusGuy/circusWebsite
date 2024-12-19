ea153bb4b04928f905169cadcf8710f8ff1fda08
Caleb H <hollandcw3@gmail.com>
2024-12-19 12:05:29 -0500

    hi

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,8 +6,8 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
-  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  apiKey: `${process.env.REACT_APP_FIREBASE_TOKEN}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_DOMAIN}`,
   databaseURL: `${process.env.REACT_APP_FIREBASE_DB_URL}`,
   projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
   storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
