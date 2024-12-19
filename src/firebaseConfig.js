dc2ec2611a4485e8a2f2e5803b97f65401f7f804
Caleb H <hollandcw3@gmail.com>
2024-12-19 11:57:53 -0500

    hehe

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -6,14 +6,14 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCTChVHJmzXadHVEb2jdXeRAiXNeLAuxI4",
-  authDomain: "eventbritecircus.firebaseapp.com",
-  databaseURL: "https://eventbritecircus-default-rtdb.firebaseio.com",
-  projectId: "eventbritecircus",
-  storageBucket: "eventbritecircus.firebasestorage.app",
-  messagingSenderId: "696567894305",
-  appId: "1:696567894305:web:3910aaa7039948d1152e60",
-  measurementId: "G-S8RHVQ57KB"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_APP_FIREBASE_DB_URL}`,
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`
 };
 
 // Initialize Firebase
